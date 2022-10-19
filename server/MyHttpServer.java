d72245f9e28028133105e3c49db5b5c7e84f445d
JFerreira03 <116104328+JFerreira03@users.noreply.github.com>
2022-10-20 00:08:52 +0100

    Update MyHttpServer.java
    coloquei o hashMap, alterei o parametro do taskSolver

--- server/MyHttpServer.java
+++ server/MyHttpServer.java
@@ -2,21 +2,23 @@ package mp1;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.HashMap;
 import request.Handler;
+import request.Util;
 
 public class MyHttpServer {
     public static void main(String[] args) throws IOException {
-        final String PORT = System.getenv().getOrDefault("SERVER_PORT", "8081");;
+        final String PORT = System.getenv().getOrDefault("SERVER_PORT", "8080");;
         ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT));
         int id = 0;
+        
+        HashMap<String, String> contentDirectory = Util.getContentDirectory();
+
         while (true) {
-            Handler taskSolver = new Handler(
-                serverSocket.accept(),
-                id
-            );
+            Handler taskSolver = new Handler(serverSocket.accept(), id, contentDirectory);
             id++;
             taskSolver.start();
         }
     }
-} 
+
+}
