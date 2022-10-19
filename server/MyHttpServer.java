f40e227eced9fb4d4b3a318d332bee88f4128c33
Rafacorreia0611 <116095502+Rafacorreia0611@users.noreply.github.com>
2022-10-20 00:07:57 +0100

    Update MyHttpServer.java

--- server/MyHttpServer.java
+++ server/MyHttpServer.java
@@ -3,20 +3,25 @@ package mp1;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+
 import request.Handler;
 
 public class MyHttpServer {
     public static void main(String[] args) throws IOException {
-        final String PORT = System.getenv().getOrDefault("SERVER_PORT", "8081");;
+        final String PORT = System.getenv().getOrDefault("SERVER_PORT", "81");
         ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT));
+        List<Integer> idRegistry = new LinkedList<>();
         int id = 0;
         while (true) {
             Handler taskSolver = new Handler(
                 serverSocket.accept(),
-                id
+                id,idRegistry
             );
             id++;
             taskSolver.start();
         }
     }
 } 
+
