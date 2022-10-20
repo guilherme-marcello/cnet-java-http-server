5be1f64e968f9863a932e9e7ad9dab46f003609b
Guilherme Marcelo <guilherme.marcelo@tekever.com>
2022-10-20 19:15:32 +0100

    Added payload read from buffer

--- server/request/RequestInfo.java
+++ server/request/RequestInfo.java
@@ -5,19 +5,28 @@ import java.io.IOException;
 import java.util.List;
 import java.util.LinkedList;
 
-
 public class RequestInfo {
     private RequestLine requestLine;
     private final List<String> requestHeaders;
+    private final String payload;
     public RequestInfo(BufferedReader in) throws IOException {
         this.requestLine = new RequestLine(in.readLine());
         this.requestHeaders = new LinkedList<>();
 
-        String header = in.readLine();
-        while ( header != null && header.length() > 0) {
+        String header;
+        while ((header = in.readLine()) != null) {
+            if (header.length() == 0)
+                break;
             this.requestHeaders.add(header);
-            header = in.readLine();
         }
+        
+        StringBuilder payloadComposer = new StringBuilder();
+        int dataChar;
+        while (in.ready()) {
+            dataChar = in.read();
+            payloadComposer.append((char) dataChar);
+        }
+        this.payload = payloadComposer.toString();
     }
     
     public RequestLine getRequestLine() {
@@ -26,6 +35,10 @@ public class RequestInfo {
     
     public List<String> getRequestHeaders() {
     	return new LinkedList<String>(requestHeaders);
+    }
+
+    public String getPayload() {
+        return this.payload;
     }
     
     @Override
