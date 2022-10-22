42028e1f88116cd36bbd14dc8860f7a00a1f72a0
guilherme-marcello <guilemosm@gmail.com>
2022-10-22 15:48:26 +0100

    Added RquestLine Model

---
+++ server/request/Models.java
@@ -0,0 +1,12 @@
+package request;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Models {
+    public static final Pattern VALID_REQUEST_LINE = Pattern.compile("[A-Z]{3,} /.{0,} HTTP/1.[0-1]\\r\\n"); 
+
+    public static boolean isRequestLineValid(String requestLine) {
+        Matcher requestLineMatcher = VALID_REQUEST_LINE.matcher(requestLine);
+        return requestLineMatcher.matches();
+    }
+}
