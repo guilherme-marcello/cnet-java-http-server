73d3c1a3c7fc12327d30f54e16724655a20862f2
guilherme-marcello <guilemosm@gmail.com>
2022-10-22 16:23:00 +0100

    Added model for header

--- server/request/Models.java
+++ server/request/Models.java
@@ -4,9 +4,16 @@ import java.util.regex.Matcher;
 
 public class Models {
     public static final Pattern VALID_REQUEST_LINE = Pattern.compile("[A-Z]{3,} /.{0,} HTTP/1.[0-1]\\r\\n"); 
+    public static final Pattern VALID_HEADER = Pattern.compile(".{1,}: .{1,}\\r\\n");
+
 
     public static boolean isRequestLineValid(String requestLine) {
         Matcher requestLineMatcher = VALID_REQUEST_LINE.matcher(requestLine);
         return requestLineMatcher.matches();
+    }
+
+    public static boolean isHeaderValid(String header) {
+        Matcher headerMatcher = VALID_HEADER.matcher(header);
+        return headerMatcher.matches();
     }
 }
