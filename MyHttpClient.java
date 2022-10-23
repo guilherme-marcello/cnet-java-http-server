7bfb7f684adb96308eca11570d1a457286996f6d
guilherme-marcello <guilemosm@gmail.com>
2022-10-23 21:41:58 +0100

    Fixed all client requests

--- MyHttpClient.java
+++ MyHttpClient.java
@@ -3,12 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-
-import java.net.SocketException;
-
-
 
 public class MyHttpClient {
 	
@@ -28,46 +23,48 @@ public class MyHttpClient {
 	}
 
 	public void getResource(String ObjectName) throws IOException {
-		String content = "GET /" + ObjectName + " HTTP/1.1" + Util.CRLF + "Host: " + this.HOST_NAME + ":" + String.valueOf(this.PORT) + Util.CRLF;
-		out.print(content);
+		out.print(
+			String.format(
+				"GET /%s HTTP/1.1%sHost: %s%s",
+				ObjectName, Util.CRLF, this.HOST_NAME, Util.CRLF
+			)
+		);
 		out.flush();
 	}
 	
 	public void postData(String[] data) throws IOException {
-		
-		System.out.print("POST /simpleForm.html HTTP/1.1" + Util.CRLF +
-					"Host: " + HOST_NAME + Util.CRLF +
-					Util.CRLF +
-					String.format("{%s,%s}", data[0], data[1])
-		);
-
-		out.print("POST /simpleForm.html HTTP/1.1" + Util.CRLF +
-					"Host: " + HOST_NAME + Util.CRLF +
-					Util.CRLF +
-					String.format("{%s,%s}", data[0], data[1])
+		out.print(
+			String.format(
+				"POST /simpleForm.html HTTP/1.1%sHost: %s%s%s%s",
+				Util.CRLF, this.HOST_NAME, Util.CRLF, Util.CRLF, String.format("{%s,%s}", data[0], data[1])
+			)
 		);
 		out.flush();
 	}
 	
 	public void sendUnimplementedMethod (String wrongMethodName) throws IOException {
-		
-		out.println(wrongMethodName + " " + "/ " + Util.CRLF );
+		out.print(
+			String.format(
+				"%s /index.html HTTP/1.1%s",
+				wrongMethodName, Util.CRLF
+			)
+		);
+		out.flush();
 	}
 	
 	public void malformedRequest(int type) throws IOException {
-		
 		switch (type) {
-		case 1: out.println("GET /index.html HTTP/1.1" +
-				"\n Host: " + HOST_NAME + Util.CRLF);
-		case 2: out.println("GET  /index.html HTTP/1.1 " + Util.CRLF +
-				"\n Host: " + HOST_NAME + Util.CRLF);	
-		case 3: out.println("GET /index.html" + Util.CRLF +
-				"\n Host: " + HOST_NAME + Util.CRLF);
-		}
+			case 1:
+				out.print("GET /index.html HTTP/1.1");
+			case 2: 
+				out.print("GET  /index.html  HTTP/1.1  " + Util.CRLF);	
+			case 3: 
+				out.print("GET /index.html" + Util.CRLF);
+			}
+		out.flush();
 	}
 	
 	public void close() {
-		
 		try {
 			this.in.close();
 			this.out.close();
@@ -75,6 +72,5 @@ public class MyHttpClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
