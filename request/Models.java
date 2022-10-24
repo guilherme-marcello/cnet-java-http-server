ceff95cf00f30e3784cc29cd97e08cb267d76649
JFerreira03 <116104328+JFerreira03@users.noreply.github.com>
2022-10-24 18:58:22 +0100

    added isPayLoadValid function

--- request/Models.java
+++ request/Models.java
@@ -1,11 +1,10 @@
-package request;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Models {
     public static final Pattern VALID_REQUEST_LINE = Pattern.compile("[A-Z]{3,} /.{0,} HTTP/1.[0-1]\\r\\n"); 
-    public static final Pattern VALID_HEADER = Pattern.compile(".{1,}: .{1,}\\r\\n");
-    public static final Pattern SIMPLEFORM_PAYLOAD_FORMAT = Pattern.compile("StudentName:.{0,},StudentId:.{0,}");
+    public static final Pattern VALID_HEADER = Pattern.compile(".{1,}: .{1,}\r\n");
+    public static final Pattern SIMPLEFORM_PAYLOAD_FORMAT = Pattern.compile("[{]StudentName:.{0,},StudentID:.{0,}[}]");
 
     public static boolean isRequestLineValid(String requestLine) {
         Matcher requestLineMatcher = VALID_REQUEST_LINE.matcher(requestLine);
@@ -15,5 +14,10 @@ public class Models {
     public static boolean isHeaderValid(String header) {
         Matcher headerMatcher = VALID_HEADER.matcher(header);
         return headerMatcher.matches();
+    }
+    
+    public static boolean isPayLoadValid (String payload) {
+    	Matcher payloadMatcher = SIMPLEFORM_PAYLOAD_FORMAT.matcher(payload);
+        return payloadMatcher.matches();
     }
 }
