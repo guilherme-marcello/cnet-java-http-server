961a6fa30cc1c113f3a85b8a48b040cdb573ec63
JFerreira03 <116104328+JFerreira03@users.noreply.github.com>
2022-10-25 20:52:13 +0100

    documentação da classe util

--- request/Util.java
+++ request/Util.java
@@ -7,9 +7,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 
+/**
+ * this class provides useful utilities 
+ */
 public class Util {
+	/**
+	 * string with the carriage return and line-feed
+	 */
 	public static final String CRLF = "\r\n";
 	
+	/**
+	 * returns a hashMap where the key are the names of the files in a 
+	 * given directory and the values are the files content
+	 * @param directory the name of the directory to map
+	 * @return hashMap of the file in a given directory
+	 * @throws IOException
+	 */
 	public static HashMap<String, String> getContentDirectory (String directory) throws IOException {
 		HashMap<String, String> contentDirectory = new HashMap<String, String>();
    
