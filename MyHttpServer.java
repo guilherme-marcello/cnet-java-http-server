602d9adeca8d11bd8d74ea0338863771d78bc24c
JFerreira03 <116104328+JFerreira03@users.noreply.github.com>
2022-10-25 22:23:04 +0100

    Documentação MyhttpServer

--- MyHttpServer.java
+++ MyHttpServer.java
@@ -8,13 +8,18 @@ import request.Util;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ *this class runs a httpServer
+ */
 public class MyHttpServer {
     public static void main(String[] args) throws IOException {
+        //number of the port to access
         final String PORT = System.getenv().getOrDefault("SERVER_PORT", "8080");;
         ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT));
+        //threads running
         List<Integer> idRegistry = new LinkedList<>();
         int id = 0;
-        
+        //hashMap with the files stored in the server
         HashMap<String, String> contentDirectory = Util.getContentDirectory("directory");
         while (true) {
             Handler taskSolver = new Handler(serverSocket.accept(), id, contentDirectory, idRegistry);
