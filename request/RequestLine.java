984315787c45956ec0b3f2581a84a937135d6714
JFerreira03 <116104328+JFerreira03@users.noreply.github.com>
2022-10-25 22:37:11 +0100

    Documentation

--- request/RequestLine.java
+++ request/RequestLine.java
@@ -1,10 +1,20 @@
 package request;
 
+/**
+ * this class provides methods to parse information 
+ * in the request line in a http request
+ */
 public class RequestLine {
     private String method = null;
     private String endpoint = null;
     private String version = null; 
-    private boolean isValid = false;;
+    private boolean isValid = false;
+
+    /**
+     * Creates a RequestLine object defined by a string, method, the method requested,
+     *  a string, endpoint, ex: /index.html and a string, the http version 
+     * @param content the content of the line request
+     */
     public RequestLine(String content) {
         if (content != null && Models.isRequestLineValid(content)) {
             this.isValid = true;
@@ -15,33 +25,39 @@ public class RequestLine {
         }  
     }
   
-    /**
-	 * @return the method
+        /**
+	 * @return this method
 	 */
 	public String getMethod() {
 		return method;
 	}
 
-
 	/**
-	 * @return the endpoint
+	 * @return this endpoint
 	 */
 	public String getEndpoint() {
 		return endpoint;
 	}
 
-
 	/**
-	 * @return the version
+	 * @return this version
 	 */
 	public String getVersion() {
 		return version;
 	}
 
+	/**
+	 * Checks if this RequestLine is a valid http request line 
+	 * @return true if this RequestLine is a valid http request
+	 * line false if otherwise
+	 */
     public boolean isValid() {
         return this.isValid;
     }
-
+	
+     /**
+     * returns a text representation of this RequestLine
+     */
 	@Override
     public String toString() {
         return String.format(
